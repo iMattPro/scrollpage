675e9ecedacc5d91cf6323da6619a7266ce11ca7
Matt Friedman <maf675@gmail.com>
2025-10-07 10:53:34 -0700

    Use CSS for faade in out transitions

--- styles/all/template/scrollpage.js
+++ styles/all/template/scrollpage.js
@@ -2,36 +2,26 @@
 	'use strict';
 
 	const settings = {
-		min: 25,
-		scrollSpeed: 400,
-		fadeSpeed: 300
+		min: 25
 	};
 
 	let buttonHidden = true;
 	const button = document.querySelector('.scroll-page');
 
 	window.addEventListener('scroll', () => {
-		const pos = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+		const pos = window.scrollY || document.documentElement.scrollTop;
 		if (pos > settings.min && buttonHidden) {
-			button.style.display = 'block'; // Show the button as a block element
-			setTimeout(() => {
-				button.style.opacity = '0.5'; // Fade in the button after it's visible
-			}, 0);
+			button.classList.add('visible');
 			buttonHidden = false;
 		} else if (pos <= settings.min && !buttonHidden) {
-			button.style.opacity = '0'; // Fade out the button
-			setTimeout(() => {
-				button.style.display = 'none'; // Hide the button after it's fully transparent
-			}, settings.fadeSpeed);
+			button.classList.remove('visible');
 			buttonHidden = true;
 		}
 	});
 
 	const scrollPage = (event) => {
-		const isScrollUp = event.currentTarget.classList.contains('scroll-up');
-		const scrollTop = isScrollUp ? 0 : document.documentElement.scrollHeight;
 		window.scrollTo({
-			top: scrollTop,
+			top: event.currentTarget.classList.contains('scroll-up') ? 0 : document.documentElement.scrollHeight,
 			behavior: 'smooth'
 		});
 	};
@@ -39,6 +29,5 @@
 	const scrollPageIcons = document.querySelectorAll('.scroll-page > i');
 	scrollPageIcons.forEach(icon => {
 		icon.addEventListener('click', scrollPage);
-		icon.addEventListener('touchstart', scrollPage);
 	});
 })();
